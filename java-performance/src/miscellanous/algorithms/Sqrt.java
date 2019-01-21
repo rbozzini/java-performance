e15983189e1c51a34983dfc7500146cbd58f2082
Rossella Bozzini <r.bozzini@internal.ebworld>
2019-01-21 17:10:20 +0100

    Test di GWT

--- java-performance/src/miscellanous/algorithms/Sqrt.java
+++ java-performance/src/miscellanous/algorithms/Sqrt.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Sqrt {
+	
+	private static BigDecimal TWO = new BigDecimal(2);
+	private static BigDecimal ERROR = new BigDecimal(0.0001);
 
 	public static BigDecimal sqrtBabylonian(BigDecimal bd) {
 		
@@ -17,8 +20,18 @@ public class Sqrt {
 	        diff = div.subtract(initial).abs();
 	        diff.setScale(8, RoundingMode.FLOOR);
 	        initial = div;
-	    } while (diff.compareTo(error) > 0);
+	    } while (diff.compareTo(ERROR) > 0);
 	    
 	    return initial;
+	}
+	
+	public static void main(String[] args) {
+		
+		BigDecimal sqrt = null;
+		BigDecimal bd = null;
+		
+		bd = new BigDecimal(7);
+		sqrt = Sqrt.sqrtBabylonian(bd);
+		System.out.println("sqrt(" + bd + ") = " + sqrt);
 	}
 }
