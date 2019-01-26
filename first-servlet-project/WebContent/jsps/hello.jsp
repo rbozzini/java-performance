ddca4150d47a54a3c09a28b08a0eaed9f3febcb7
rbozzini <rossellabozzini@gmail.com>
2019-01-26 22:01:59 +0100

    Servlet JSP tutorial

---
+++ first-servlet-project/WebContent/jsps/hello.jsp
@@ -0,0 +1,16 @@
+<%@page import="java.util.Date"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello</title>
+</head>
+<body>
+<h2>Ciao Arianna!!!</h2>
+<br>
+<h3>Date=<%= new Date() %>
+</h3>
+</body>
+</html>
---
+++ servlet-cookie-example/src/it/dev/servlet/session/LogoutServlet.java
@@ -0,0 +1,42 @@
+package it.dev.servlet.session;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		Cookie loginCookie = null;
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("user")) {
+					loginCookie = cookie;
+					break;
+				}
+			}
+		}
+		if (loginCookie != null) {
+			// There is no method to remove the cookie but we can set the maximum age to 0
+			// so that it will be deleted from client browser immediately.
+			loginCookie.setMaxAge(0);
+			response.addCookie(loginCookie);
+		}
+		// If we try to access the JSP directly, it will forward us to the login page.
+		response.sendRedirect("login.html");
+	}
+
+}
