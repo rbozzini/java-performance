2962c2820c6b690b68ff76f2c1b20138be9b700d
rbozzini <rossellabozzini@gmail.com>
2019-04-08 21:45:53 +0200

    La concorrenza in java

---
+++ examples/src/it/corsojava/concorrenza/GetSitePageForkJoin.java
@@ -0,0 +1,60 @@
+package it.corsojava.concorrenza;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.RecursiveTask;
+
+public class GetSitePageForkJoin extends RecursiveTask<String> {
+
+	private String url;
+
+	public GetSitePageForkJoin(String url) {
+		super();
+		this.url = url;
+	}
+
+	@Override
+	protected String compute() {
+		try {
+			URL u = new URL(url);
+
+			URLConnection connection = u.openConnection();
+
+			InputStream is = connection.getInputStream();
+
+			return readFileContent(is);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private String readFileContent(InputStream is) {
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+
+		br = new BufferedReader(new InputStreamReader(is));
+		String line;
+		try {
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return sb.toString();
+	}
+
+}
