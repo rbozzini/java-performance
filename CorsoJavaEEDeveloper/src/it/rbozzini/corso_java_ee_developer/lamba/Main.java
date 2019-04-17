1100e551130aea442c3d98aa4aaf69b34b7a4630
bozzinir <rossella.bozzini@tasgroup.eu>
2019-04-17 18:01:35 +0200

    Consumer

--- CorsoJavaEEDeveloper/src/it/rbozzini/corso_java_ee_developer/lamba/Main.java
+++ CorsoJavaEEDeveloper/src/it/rbozzini/corso_java_ee_developer/lamba/Main.java
@@ -25,22 +25,6 @@ public class Main {
 		trovati.forEach(printCons);
 	}
 
-	private List<Utente> elencoUtenti() {
-		List<Utente> utenti = new ArrayList<Utente>();
-
-		utenti.add(new Utente("Rossella", "Bozzini", 45, "Verona", "rossellabozzini@gmail.com", "test"));
-		utenti.add(new Utente("Denis", "Brandolini", 45, "Ravenna", "denis.brandolini@gmail.com", "test"));
-		utenti.add(new Utente("Valentina", "Brandolini", 9, "Bologna", "vale.brandolini@gmail.com", "test"));
-		utenti.add(new Utente("Arianna", "Brandolini", 6, "Bologna", "ari.brandolini@gmail.com", "test"));
-		utenti.add(new Utente("Amelia", "Paparini", 70, "Mantova", "titti.paparini@gmail.com", "test"));
-		utenti.add(new Utente("Martina", "Bozzini", 39, "Venezia", "martina.bozzini@gmail.com", "test"));
-		utenti.add(new Utente("Rosalba", "Buscherini", 74, "Filetto", "rosalba.buscherini@gmail.com", "test"));
-		utenti.add(new Utente("Diego", "Romani", 40, "Verona", "diego.romani@gmail.com", "test"));
-		utenti.add(new Utente("Riccardo", "Romani", 4, "Bologna", "riccardo.romani@gmail.com", "test"));
-
-		return utenti;
-	}
-
 	public List<Utente> cercaUtenti(List<Utente> utenti, Predicate<Utente> p) {
 
 		List<Utente> utentiTrovati = new ArrayList<Utente>();
@@ -52,6 +36,22 @@ public class Main {
 		}
 
 		return utentiTrovati;
+	}
+	
+	private List<Utente> elencoUtenti() {
+		List<Utente> utenti = new ArrayList<Utente>();
+		
+		utenti.add(new Utente("Valentina", "Brandolini", 9, "Bologna", "vale.brandolini@gmail.com", "test"));
+		utenti.add(new Utente("Arianna", "Brandolini", 6, "Bologna", "ari.brandolini@gmail.com", "test"));
+		utenti.add(new Utente("Amelia", "Paparini", 70, "Mantova", "titti.paparini@gmail.com", "test"));
+		utenti.add(new Utente("Martina", "Bozzini", 39, "Venezia", "martina.bozzini@gmail.com", "test"));
+		utenti.add(new Utente("Rosalba", "Buscherini", 74, "Filetto", "rosalba.buscherini@gmail.com", "test"));
+		utenti.add(new Utente("Diego", "Romani", 40, "Verona", "diego.romani@gmail.com", "test"));
+		utenti.add(new Utente("Riccardo", "Romani", 4, "Bologna", "riccardo.romani@gmail.com", "test"));
+		utenti.add(new Utente("Rossella", "Bozzini", 45, "Verona", "rossellabozzini@gmail.com", "test"));
+		utenti.add(new Utente("Denis", "Brandolini", 45, "Ravenna", "denis.brandolini@gmail.com", "test"));
+		
+		return utenti;
 	}
 
 }
